c89b922fc53ed89fe4dc499e181169a1fb4f9ad5
Hanneszz <118542657+Hanneszz@users.noreply.github.com>
2023-03-25 19:15:15 +0100

    added depth iteration

---
+++ iterate_depths.m
@@ -0,0 +1,59 @@
+% Iterate through depth 
+clear
+close
+
+start_depth = 0;
+end_depth = 0.125;
+step_size = 0.005;
+num_steps = (end_depth - start_depth)./step_size;
+
+%% Plot options
+show_geometry = false;
+show_direction = false;
+
+show_f_quiver = false;
+show_alpha = false;
+
+show_f_scatter = false;
+show_f_scatterxyz = true;
+
+show_linear_f = false;
+
+saveFigures = false;
+
+
+sim_data_dtos = SimDataTransferObject.empty(0,num_steps);
+
+step = 1;
+depths = start_depth:step_size:end_depth;
+z = zeros(1, num_steps);
+
+for cur_depth = start_depth:step_size:end_depth
+    sim_data_dtos(step) = RFT3D(cur_depth);
+    z(step) = sim_data_dtos(step).forces_z;
+    step = step + 1;
+end
+
+c_inc = sim_data_dtos(end).c_inc;
+v_norm_vec = sim_data_dtos(end).v_norm_vec;
+F = sim_data_dtos(end).F;
+f = sim_data_dtos(end).f;
+forces_x = sim_data_dtos(end).forces_x;
+forces_y = sim_data_dtos(end).forces_y;
+forces_z = sim_data_dtos(end).forces_z;
+forces = sim_data_dtos(end).forces;
+T = sim_data_dtos(end).T;
+torque_x = sim_data_dtos(end).torque_x;
+torque_y = sim_data_dtos(end).torque_y;
+torque_z = sim_data_dtos(end).torque_z;
+alpha_gen = sim_data_dtos(end).alpha_gen;
+alpha_gen_n = sim_data_dtos(end).alpha_gen_n;
+alpha_gen_t = sim_data_dtos(end).alpha_gen_t;
+alpha = sim_data_dtos(end).alpha;
+TRG = sim_data_dtos(end).TRG;
+TRG_visual = sim_data_dtos(end).TRG_visual;
+
+RFTPlots
+
+figure
+plot(depths, z)
